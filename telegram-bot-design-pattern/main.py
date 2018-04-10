80c9a4c4bd3d66b1e6d7781b1391d76f3550ba7f
lucaoflaif <d.v.luca99@gmail.com>
2018-04-10 18:33:16 +0200

    fix import bug

--- telegram-bot-design-pattern/main.py
+++ telegram-bot-design-pattern/main.py
@@ -1,9 +1,12 @@
 """Bot start file"""
 import os
 from env import ENV as env
-from db import DB_CONNECTION
 from core import Bot
 
+try:
+    from db import DB_CONNECTION
+except ImportError:
+    DB_CONNECTION = None
 
 if __name__ == "__main__":
     bot = Bot(env=env, db=DB_CONNECTION)
